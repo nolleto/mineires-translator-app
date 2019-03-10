003f9f9b7a05bd56da0e777805a81bdff3e6e7ce
nolleto <felipenolletonascimento@gmail.com>
2019-03-10 15:56:23 -0300

    Updates

---
+++ precache-manifest.acc8f334d0425f026331d2d39bb47c2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b52e451a7c7b3b1646ba",
+    "url": "/mineires-translator-app/static/js/runtime~main.b52e451a.js"
+  },
+  {
+    "revision": "f63ed162f6dbb3523dc3",
+    "url": "/mineires-translator-app/static/js/main.f63ed162.chunk.js"
+  },
+  {
+    "revision": "c79f88fbdeaeda20e3f6",
+    "url": "/mineires-translator-app/static/js/2.c79f88fb.chunk.js"
+  },
+  {
+    "revision": "f63ed162f6dbb3523dc3",
+    "url": "/mineires-translator-app/static/css/main.f89f7180.chunk.css"
+  },
+  {
+    "revision": "c23b90438933ccacea1979553308c5cc",
+    "url": "/mineires-translator-app/index.html"
+  }
+];
